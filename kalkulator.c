a19ceaffa116f2f8214b3c4579d2760f6c2111a8
hubjancu <80957720+hubjancu@users.noreply.github.com>
2021-03-31 18:41:04 +0200

    Create kalkulator.c

---
+++ kalkulator.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+int  main()
+{
+
+float a,b;
+char znak;
+
+printf("Wprowadz rownanie np a + b ");
+scanf("%f %c %f", &a, &znak, &b);
+
+switch(znak)
+{
+        case '+':
+        printf("\n%.2f + %.1f = %.2f ", a , b , a+b);
+        break;
+        case '-':
+        printf("\n%.2f - %.3f = %.2f ", a , b , a-b);
+        break;
+        case '*':
+        printf("\n%.2f * %.1f = %.2f ", a , b , a*b);
+        break;
+        case '/':
+        printf("\n%.2f / %.2f = %.2f\n ", a , b , a/b);
+        break;
+        case '%':
+        printf("\n modulo = %d", (int)a%(int)b);
+        break;
+
+
+        default :
+        printf("zle wprowadzone rownanie");
+        break;
+}
+return 0;
+}
+
